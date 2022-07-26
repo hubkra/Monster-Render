d1c0867ab784e880404dfa89b2ccaabeb320d6e8
Hubert Kras <mrkrashubert@gmail.com>
2022-07-26 19:08:58 +0200

    Monster Render - Course Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
+
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import CardList from './card-list/card-list.component';
+import SearchBox from  './search-box/search-box.component'
 
-function App() {
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters:[],
+      searchField: '',
+    };
+  }
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(()=>{
+        return {monsters: users}
+
+      }, 
+      ()=>{
+        console.log(this.state)
+      }))
+
+    }
+
+    onSearchChange = (event)=>{
+      const searchField = event.target.value.toLocaleLowerCase();
+      this.setState(()=> {
+         return {searchField};
+       })}
+
+  render(){
+    const {monsters,searchField}=  this.state;
+    const  {onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster)=>{ 
+      return monster.name.toLocaleLowerCase().includes(searchField);
+     });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className='app-title'>Monsters Render</h1>
+    <SearchBox onSearchChangeHandler={onSearchChange} placeholder='Search Monster' className='search-box'/>
+      
+      <CardList monsters ={filteredMonsters} />
     </div>
+    
   );
+  }
 }
-
 export default App;
+  
